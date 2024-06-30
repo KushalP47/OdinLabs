e8a0772d53266f0de885c68e5f7d099e30daf268
harsh.b2 <harsh.b2@ahduni.edu.in>
2024-06-30 09:02:20 +0000

    update: full screen button added to contest page

--- client/src/pages/Contest.tsx
+++ client/src/pages/Contest.tsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import { useEffect } from "react";
 
 const Contest = () => {
+	const [fullscreen, setFullscreen] = useState(false);
 
+	const handleFullscreenChange = () => {
+		setFullscreen(!!document.fullscreenElement);
+	};
+
+	//add the event listener to the document
 	useEffect(() => {
-		
-	}, []);
+		document.addEventListener("fullscreenchange", handleFullscreenChange);
+		return () => {
+			document.removeEventListener("fullscreenchange", handleFullscreenChange);
+		};
+	});
 
-	
+	const toggleFullscreen = () => {
+		if (!document.fullscreenElement) {
+			document.documentElement.requestFullscreen().catch((err) => {
+				console.error("Error attempting to enable full screen:", err.message);
+			});
+		} else {
+			document.exitFullscreen();
+		}
+	};
+
+
 	return (
-		<div className="flex min-h-screen">
+		<div className={`flex min-h-screen ${fullscreen ? "fullscreen" : ""}`}>
 			{/* Navbar */}
 			<Navbar currentPage="Contest" />
 
@@ -20,9 +39,17 @@ const Contest = () => {
 					<h1 className="text-4xl font-bold">Contest</h1>
 				</div>
 
+				{/* Fullscreen Button */}
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded"
+					onClick={toggleFullscreen}
+				>
+					{fullscreen ? "Exit Fullscreen" : "Fullscreen"}
+				</button>
+
 				{/* Content Section */}
 				<div className="flex-1 overflow-auto">
-					
+					{/* Your content here */}
 				</div>
 			</div>
 		</div>
