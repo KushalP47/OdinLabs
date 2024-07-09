79e838bbee22b146c48ebbab8a5c90c724af16dd
Harsh Bhagat <habhagat2003@gmail.com>
2024-07-09 14:59:34 +0530

    fix: java parsing error parsing

--- client/src/components/Editor/OutputWindow.tsx
+++ client/src/components/Editor/OutputWindow.tsx
@@ -15,7 +15,7 @@ const OutputWindow = ({ outputDetails, language_id }: OutputWindowProps) => {
 						: null}
 
 					{language_id === 62 && outputDetails.compile_output !== null
-						? `${atob(outputDetails.stdout)}`
+						? `${atob(outputDetails.compile_output)}`
 						: null}
 
 					{language_id === 71 && outputDetails.compile_output !== null
