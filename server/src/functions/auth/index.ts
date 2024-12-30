8f0f816be01384cada97a05479a4289fa078dcce
Kushal Patel <kushal300603@gmail.com>
2024-12-30 10:29:35 +0530

    spring boot server initialized

--- server/src/functions/auth/index.ts
+++
@@ -1,4 +0,0 @@
-import { userExists, userFromEmail } from './userExists';
-import { createUser } from './createUser';
-import { updateUserPassword } from './editUser';
-export { userExists, createUser, userFromEmail, updateUserPassword};
--- server/src/types/custom.d.ts
+++
@@ -1,9 +0,0 @@
-import { IUser } from '../models/user.model'; // Adjust the import path as necessary
-
-declare global {
-    namespace Express {
-        interface Request {
-            user?: IUser;
-        }
-    }
-}
