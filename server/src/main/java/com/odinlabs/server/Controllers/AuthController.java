8adc39667518198830b92d4c7b07c342c19c3e59
Harsh Bhagat <habhagat2003@gmail.com>
2025-01-28 20:52:24 +0530

    Update AuthController.java

--- server/src/main/java/com/odinlabs/server/Controllers/AuthController.java
+++ server/src/main/java/com/odinlabs/server/Controllers/AuthController.java
@@ -3,9 +3,7 @@ package com.odinlabs.server.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.odinlabs.server.Models.User;
 import com.odinlabs.server.Service.JwtService;
@@ -14,6 +12,7 @@ import com.odinlabs.server.Service.UserService;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/v1/auth")
 public class AuthController {
 
     @Autowired
